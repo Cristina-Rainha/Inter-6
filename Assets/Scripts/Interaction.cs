00bd8e7f84d1ddcf11ab21b9954937a44a7e1954
eishimoto <eishimoto@outlook.com>
2022-08-31 17:45:43 -0300

    change with input

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -4,8 +4,72 @@ using UnityEngine;
 using Cinemachine;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class Interaction : MonoBehaviour
 {
-  
+    //Input System
+    private PlayerInputSystem mInputSystem;
+    private InputAction XboxInput;
+    private InputAction PS4Input;
+    private InputAction KeyboardInput;
+
+    //UI
+    [SerializeField] private List<TextMeshProUGUI> UseText;
+
+    void Awake()
+    {
+        mInputSystem = new PlayerInputSystem();
+    }
+    void OnEnable()
+    {
+        XboxInput = mInputSystem.UI.XboxInput;
+        XboxInput.Enable();
+        XboxInput.performed += OpenTextBox;
+        
+        PS4Input = mInputSystem.UI.PS4Input;
+        PS4Input.Enable();
+        PS4Input.performed += OpenTextBoxPS;
+        
+        KeyboardInput = mInputSystem.UI.KeyboardInput;
+        KeyboardInput.Enable();
+        KeyboardInput.performed += OpenTextBoxKB;
+    }
+    void OnDisable()
+    {
+        XboxInput.Disable();
+        PS4Input.Disable();
+        KeyboardInput.Disable();
+    }
+    private void OpenTextBox(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+        {
+            foreach (TextMeshProUGUI text in UseText)
+            {
+                text.text = "B";
+            }
+        }
+    }
+    private void OpenTextBoxPS(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+        {
+            foreach (TextMeshProUGUI text in UseText)
+            {
+                text.text = "O";
+            }
+        }
+    }
+
+    private void OpenTextBoxKB(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+        {
+            foreach (TextMeshProUGUI text in UseText)
+            {
+                text.text = "E";
+            }
+        }
+    }
 }
