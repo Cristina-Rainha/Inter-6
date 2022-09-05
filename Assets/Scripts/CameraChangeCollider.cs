a7e7f84c043982b633f272119864d9e6a76a0eab
eishimoto <eishimoto@outlook.com>
2022-09-05 09:39:49 -0300

    change cam

--- Assets/Scripts/CameraChangeCollider.cs
+++ Assets/Scripts/CameraChangeCollider.cs
@@ -9,6 +9,8 @@ public class CameraChangeCollider : MonoBehaviour
     [SerializeField] GameObject cam1;
     [SerializeField] GameObject cam2;
     [SerializeField] CinemachineVirtualCamera virtualCamera;
+    [SerializeField] float camChangeTime = 0.5f;
+
     private void Start()
     {
         cam2.SetActive(false);
@@ -38,20 +40,32 @@ public class CameraChangeCollider : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
-            cam1.SetActive(false);
-            cam2.SetActive(true);
-            myCamera.orthographic = false;
+            StartCoroutine(InsideCam());
         }
     }
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            cam1.SetActive(true);
-            cam2.SetActive(false);
-            myCamera.orthographic = true;
-            virtualCamera.m_Lens.NearClipPlane = -1000;
-            virtualCamera.m_Lens.FarClipPlane = 5000;
+            StartCoroutine(OutsideCam());
         }
+    }
+
+    IEnumerator InsideCam()
+    {
+        yield return new WaitForSeconds(camChangeTime);
+        cam1.SetActive(false);
+        cam2.SetActive(true);
+        myCamera.orthographic = false;
+    }
+
+    IEnumerator OutsideCam()
+    {
+        yield return new WaitForSeconds(camChangeTime);
+        cam1.SetActive(true);
+        cam2.SetActive(false);
+        myCamera.orthographic = true;
+        virtualCamera.m_Lens.NearClipPlane = -1000;
+        virtualCamera.m_Lens.FarClipPlane = 5000;
     }
 }
