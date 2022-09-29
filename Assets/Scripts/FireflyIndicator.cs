652d41f8a3af3ddedcbae8d28722b130ded0ef66
eishimoto <eishimoto@outlook.com>
2022-09-29 16:54:56 -0300

    Firefly

--- Assets/Scripts/FireflyIndicator.cs
+++ Assets/Scripts/FireflyIndicator.cs
@@ -4,22 +4,35 @@ using UnityEngine;
 
 public class FireflyIndicator : MonoBehaviour
 {
-    [SerializeField] private GameObject fireflyPink;
-    [SerializeField] private GameObject fireflyRed;
+    [SerializeField] private ParticleSystem fireflyParticles;
+    [SerializeField] private Light fireflyLight;
+    [SerializeField] private Transform RedNpc;
+    [SerializeField] private Transform PinkNpc;
+
+    private bool Redarea = false;
+    private bool Pinkarea = false;
     void Start()
     {
+        fireflyParticles.Stop();
+        fireflyLight.intensity = 1;
     }
 
+    private void Update()
+    {
+        RedQuest();
+        PinkQuest();
+    }
     private void OnTriggerEnter(Collider other)
     {
-        
         if (other.CompareTag("pinkArea"))
         {
             if (VariableHolder.pinkQuest == false)
             {
-                fireflyPink.SetActive(true);
-                StartCoroutine(StopFireFlys());
-                Debug.Log("in pink zone");
+                var main = fireflyParticles.main;
+                fireflyParticles.Play();
+                main.startColor = new Color(255, 0, 255);
+                fireflyLight.intensity = 1;
+                Pinkarea = true;
             }
         }
 
@@ -27,9 +40,11 @@ public class FireflyIndicator : MonoBehaviour
         {
             if (VariableHolder.redQuest == false)
             {
-                fireflyRed.SetActive(true);
-                StartCoroutine(StopFireFlys());
-                Debug.Log("in red zone");
+                var main = fireflyParticles.main;
+                fireflyParticles.Play();
+                main.startColor = new Color(255, 0, 0);
+                fireflyLight.intensity = 1;
+                Redarea = true;
             }
         }
     }
@@ -38,21 +53,91 @@ public class FireflyIndicator : MonoBehaviour
     {
         if (other.CompareTag("pinkArea"))
         {
-            fireflyPink.SetActive(false);
-            Debug.Log("out pink zone");
+            fireflyParticles.Stop();
+            Pinkarea = false;
+
         }
 
         if (other.CompareTag("redArea"))
         {
-            fireflyRed.SetActive(false);
-            Debug.Log("out redArea zone");
+            fireflyParticles.Stop();
+            Redarea = false;
         }
     }
-
-    IEnumerator StopFireFlys()
+    private void RedQuest()
     {
-        yield return new WaitForSeconds(6);
-        fireflyPink.SetActive(false);
-        fireflyRed.SetActive(false);
+        float distance = Vector3.Distance(RedNpc.transform.position, transform.position);
+        if (Redarea)
+        {
+            //Debug.Log("red quest" + distance);
+            if(distance < 40)
+            {
+                fireflyLight.intensity = 5;
+            }
+            
+            if(distance < 30)
+            {
+                fireflyLight.intensity = 20;
+            }
+
+            if (distance < 20)
+            {
+                fireflyLight.intensity = 40;
+            }
+
+            if (distance < 10)
+            {
+                fireflyLight.intensity = 60;
+            }
+
+            if (distance < 5)
+            {
+                fireflyLight.intensity = 80;
+            }
+
+            if (distance < 2)
+            {
+                fireflyParticles.Stop();
+            }
+        }
+    }
+    
+    private void PinkQuest()
+    {
+        float distance = Vector3.Distance(PinkNpc.transform.position, transform.position);
+
+        if (Pinkarea)
+        {
+            //Debug.Log("pink quest" + distance);
+            if (distance < 40)
+            {
+                fireflyLight.intensity = 5;
+            }
+
+            if (distance < 30)
+            {
+                fireflyLight.intensity = 20;
+            }
+
+            if (distance < 20)
+            {
+                fireflyLight.intensity = 40;
+            }
+
+            if (distance < 10)
+            {
+                fireflyLight.intensity = 60;
+            }
+
+            if (distance < 5)
+            {
+                fireflyLight.intensity = 80;
+            }
+
+            if (distance < 2)
+            {
+                fireflyParticles.Stop();
+            }
+        }
     }
 }
