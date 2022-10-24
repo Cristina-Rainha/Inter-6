d3b50e42254c4877a453248d339945a7ef642811
eishimoto <eishimoto@outlook.com>
2022-10-24 10:07:30 -0300

    npcs

---
+++ Assets/Scripts/NPC/NpcGreenItem2.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NpcGreenItem2 : MonoBehaviour
+{
+[SerializeField] private GameObject canvasPanel;
+    [SerializeField] private GameObject item;
+    
+    //bool
+    private bool inside;
+    //Input Action
+    private PlayerInputSystem mInputSystem;
+    private InputAction InteractInput;
+
+    private void Awake()
+    {
+        mInputSystem = new PlayerInputSystem();
+    }
+    void Start()
+    {
+    }
+    private void OnEnable()
+    {
+        InteractInput = mInputSystem.Player.Use;
+        InteractInput.Enable();
+        InteractInput.performed += collect;
+    }
+    private void OnDisable()
+    {
+        InteractInput.Disable();
+    }
+    void Update()
+    {
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inside = true;
+            canvasPanel.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inside = false;
+            canvasPanel.SetActive(false);
+        }
+    }
+    public void collect(InputAction.CallbackContext ctx)
+    {
+        if (inside && VariableHolder.greenItem2 == false)
+        {
+            VariableHolder.greenItem2 = true;
+            item.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/NPC/NpcOrange.cs
@@ -0,0 +1,131 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Cinemachine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+public class NpcOrange : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera virtualCamera;
+    [SerializeField] private GameObject canvasPanel;
+    [SerializeField] private GameObject canvasText;
+    [SerializeField] private GameObject canvasText2;
+    [SerializeField] private GameObject Item;
+
+    //bool
+    bool insideInterationZone;
+
+    //Input Action
+    private PlayerInputSystem mInputSystem;
+    private InputAction InteractInput;
+
+    NavMeshAgent agent;
+
+    [SerializeField] private List<Transform> waypoints;
+
+    private int currentWaypoint;
+    private float waitTime = 0.5f;
+
+    private Vector3 target;
+
+    private void Awake()
+    {
+        mInputSystem = new PlayerInputSystem();
+    }
+    private void OnEnable()
+    {
+        InteractInput = mInputSystem.Player.Use;
+        InteractInput.Enable();
+        InteractInput.performed += OpenTextBox;
+    }
+    private void OnDisable()
+    {
+        InteractInput.Disable();
+        canvasText.SetActive(false);
+        canvasText2.SetActive(false);
+        virtualCamera.m_Lens.OrthographicSize = 6;
+    }
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        currentWaypoint = 0;
+    }
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, target) < 1f)
+        {
+            GetNextWaypoint();
+            UpdateDestination();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            insideInterationZone = true;
+            virtualCamera.m_Lens.OrthographicSize = 4;
+            canvasPanel.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            insideInterationZone = false;
+            virtualCamera.m_Lens.OrthographicSize = 6;
+            canvasPanel.SetActive(false);
+            canvasText.SetActive(false);
+            canvasText2.SetActive(false);
+        }
+    }
+
+    public void OpenTextBox(InputAction.CallbackContext ctx)
+    {
+        if (insideInterationZone)
+        {
+            canvasText.SetActive(true);
+            if (VariableHolder.orangeItem == false)
+            {
+                Item.SetActive(true);
+            }
+        }
+
+        if (insideInterationZone && VariableHolder.orangeItem == true)
+        {
+            canvasText2.SetActive(true);
+            StartCoroutine("GoAway");
+            VariableHolder.orangeQuest = true;
+        }
+    }
+
+    void UpdateDestination()
+    {
+        target = waypoints[currentWaypoint].position;
+        agent.SetDestination(target);
+
+        if (currentWaypoint == waypoints.Count - 1)
+        {
+            agent.isStopped = true;
+            gameObject.SetActive(false);
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        waitTime -= Time.deltaTime;
+        if (waitTime <= 0)
+        {
+            currentWaypoint = (currentWaypoint + 1) % waypoints.Count;
+        }
+    }
+
+    IEnumerator GoAway()
+    {
+        yield return new WaitForSeconds(3f);
+        UpdateDestination();
+    }
+}
