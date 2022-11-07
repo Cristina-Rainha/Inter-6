28e2208a3d9901dc5d4257521b1432e7e4d37644
eishimoto <eishimoto@outlook.com>
2022-11-07 14:51:51 -0300

    save

--- Assets/Scripts/CameraChangeCollider.cs
+++ Assets/Scripts/CameraChangeCollider.cs
@@ -15,27 +15,7 @@ public class CameraChangeCollider : MonoBehaviour
     {
         cam2.SetActive(false);
     }
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "null")
-        {
-            if(cam1.activeSelf)
-            { 
-                cam1.SetActive(false);
-                cam2.SetActive(true);
-                myCamera.orthographic = false;
-            }
-            else
-            {
-                cam1.SetActive(true);
-                cam2.SetActive(false);
-                myCamera.orthographic = true;
-                virtualCamera.m_Lens.NearClipPlane = -1000;
-                virtualCamera.m_Lens.FarClipPlane = 5000;
-            }
-        }
-    }//not in use
-
+   
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Player")
