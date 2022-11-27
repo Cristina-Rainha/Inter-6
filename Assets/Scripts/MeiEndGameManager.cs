fb0132885e93c5e4aec6318c3158991b026aa951
eishimoto <eishimoto@outlook.com>
2022-11-27 11:22:15 -0300

    end game "finished"

--- Assets/Scripts/MeiEndGameManager.cs
+++ Assets/Scripts/MeiEndGameManager.cs
@@ -6,9 +6,28 @@ using UnityEngine.SceneManagement;
 public class MeiEndGameManager : MonoBehaviour
 {
     [SerializeField] private int sceneToLoad;
+    [SerializeField] private GameObject endMap;
+    [SerializeField] private GameObject endGameCanvas;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
     public void LoadScene()
     {
+        StartCoroutine(LoadSceneAfterDelay());
+    }
+
+    public void DisableAndEnable()
+    {
+        endGameCanvas.SetActive(true);
+        endMap.SetActive(false);
+    }
+
+    IEnumerator LoadSceneAfterDelay()
+    {
+        yield return new WaitForSeconds(5);
         SceneManager.LoadScene(sceneToLoad);
     }
 }
