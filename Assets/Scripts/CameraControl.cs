e73b8b5a720412ce4f24f3c23d6ea3159132a9d0
eishimoto <eishimoto@outlook.com>
2022-11-26 23:48:34 -0300

    fix menu

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraControl : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera vcam;
+    void Update()
+    {
+        //vcam.
+    }
+}
