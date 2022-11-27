2c078568c75122761226670c160317873624127e
eishimoto <eishimoto@outlook.com>
2022-11-27 10:57:48 -0300

    end game

--- Assets/Scripts/MeiEndGameManager.cs
+++ Assets/Scripts/MeiEndGameManager.cs
@@ -1,8 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MeiEndGameManager : MonoBehaviour
 {
-    
+    [SerializeField] private int sceneToLoad;
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
 }
